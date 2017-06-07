0ff183adadb3e19f9321bf277fafaa2bba897e16
LOTIRELK <lorcan.james.otoole@gmail.com>
2017-06-07 12:55:54 +0100

    ju
    ju

---
+++ Lab3Question1/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Lab3Question1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInputHour = new System.Windows.Forms.TextBox();
+            this.txt12hour = new System.Windows.Forms.TextBox();
+            this.txtInputMinute = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(50, 325);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(112, 35);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "24 hour format: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 160);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "12 hour format: ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtInputHour
+            // 
+            this.txtInputHour.Location = new System.Drawing.Point(240, 63);
+            this.txtInputHour.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtInputHour.Name = "txtInputHour";
+            this.txtInputHour.Size = new System.Drawing.Size(67, 26);
+            this.txtInputHour.TabIndex = 0;
+            this.txtInputHour.Text = "Hrs";
+            this.txtInputHour.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt12hour
+            // 
+            this.txt12hour.Location = new System.Drawing.Point(240, 149);
+            this.txt12hour.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt12hour.Name = "txt12hour";
+            this.txt12hour.ReadOnly = true;
+            this.txt12hour.Size = new System.Drawing.Size(175, 26);
+            this.txt12hour.TabIndex = 5;
+            this.txt12hour.TabStop = false;
+            // 
+            // txtInputMinute
+            // 
+            this.txtInputMinute.Location = new System.Drawing.Point(351, 63);
+            this.txtInputMinute.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtInputMinute.Name = "txtInputMinute";
+            this.txtInputMinute.Size = new System.Drawing.Size(64, 26);
+            this.txtInputMinute.TabIndex = 1;
+            this.txtInputMinute.Text = "Mins";
+            this.txtInputMinute.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(318, 62);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 33);
+            this.label3.TabIndex = 7;
+            this.label3.Text = ":";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(383, 325);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(121, 35);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 402);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtInputMinute);
+            this.Controls.Add(this.txt12hour);
+            this.Controls.Add(this.txtInputHour);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalculate);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Time Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInputHour;
+        private System.Windows.Forms.TextBox txt12hour;
+        private System.Windows.Forms.TextBox txtInputMinute;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Lab3Question1/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+//a form that taes in the time in hours and minutes and outputs the time
+//in 12 hour format
+namespace Lab3Question1
+{
+    public partial class Form1 : Form
+    {
+        //set to 0
+        int inputHour = 0;
+        int inputMinute = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            inputMinute = Convert.ToInt32(txtInputMinute.Text);
+            inputHour = Convert.ToInt32(txtInputHour.Text);
+
+            //call function to check the input
+            checkInput();
+
+            //calll function to convert to 12 hour format
+            output12HourFormat();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //exit form when button clicked
+            this.Close();
+        }
+
+        //declaration of functon to output 12 hour format
+        public void output12HourFormat()
+        {
+            //variables take in strings from textboxes and convert to int
+            
+
+            //check hour and carry out process if condidtion met
+            if (inputHour == 00 && inputMinute == 00)
+            {
+                txt12hour.Text = "Midnight";
+            }
+            else if (inputHour == 12 && inputMinute == 00)
+            {
+                txt12hour.Text = "Noon";
+            }
+            else if (inputHour >= 12 && inputHour <= 23)
+            {
+                if (inputHour == 12)
+                {
+                    if (inputMinute >= 0 && inputMinute <= 9)//add leading 0 to single digit numbers
+                    {
+                        txt12hour.Text = inputHour.ToString() + ":" + '0' + inputMinute.ToString() + "pm";
+                    }
+                }
+                else
+                    inputHour -= 12;//convert 24 hour format to 12
+                if (inputMinute >= 0 && inputMinute <= 9)
+                {
+                    txt12hour.Text = inputHour.ToString() + ":" + '0' + inputMinute.ToString() + "pm";
+                }
+                else
+                    txt12hour.Text = inputHour.ToString() + ":" + inputMinute.ToString() + "pm";
+            }
+            else txt12hour.Text = inputHour.ToString() + ":" + inputMinute.ToString() + "am";
+        }
+
+        //declaration of funcion to check input was correct
+        public void checkInput()
+        {
+            if (inputHour < 0 || inputHour > 23)
+            {
+                MessageBox.Show(
+                "Hour entered must be hour on 24 hour clock",
+                "Entry Error");
+                txtInputHour.Focus();
+                this.Close();
+            }
+            else if (inputMinute < 0 || inputMinute > 59)
+            {
+                MessageBox.Show(
+                "Minutes entered must be between 0 and 59",
+                "Entry Error");
+                txtInputMinute.Focus();
+                this.Close();
+            }
+        }
+    }
+}
